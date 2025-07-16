a47252cf5af879a28f638bdb5eb93f8e5af1ad0b
le.tung.bao.an <le.tung.bao.an@sun-asterisk.com>
2025-07-16 18:15:55 +0700

    Chapter 3, 4, 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "microposts/index"
+  get "static_pages/contact"
+  get "static_pages/home"
+  get "static_pages/help"
 end
