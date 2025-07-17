fd1919b7f2a99eb279a7139da17467fa7b415d5d
le.tung.bao.an <le.tung.bao.an@sun-asterisk.com>
2025-07-17 14:34:13 +0700

    Chapter 6

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ApplicationRecord
+  has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true,
+length: {maximum: Settings.development.user.MAX_NAME_LENGTH}
+  validates :email, presence: true,
+length: {maximum: Settings.development.user.MAX_EMAIL_LENGTH},
+format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :birthday, presence: true
+  validate :birthday_within_100_years
+
+  private
+  def birthday_within_100_years
+    return if birthday.blank? || !birthday.is_a?(Date)
+
+    current_date = Time.zone.today
+    hundred_years_ago = current_date.prev_year(Settings.development
+    .user.BIRTHDAY_YEAR_LIMIT)
+
+    if birthday > current_date
+      errors.add(:birthday, :cannot_be_in_the_future)
+    elsif birthday < hundred_years_ago
+      errors.add(:birthday, :must_be_within_100_years)
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_07_16_082429) do
+ActiveRecord::Schema[7.0].define(version: 2025_07_17_070030) do
   create_table "microposts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.date "birthday"
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
