7b84c510588add413c27da6ce699be7c8d7acd24
le.tung.bao.an <le.tung.bao.an@sun-asterisk.com>
2025-07-21 11:40:34 +0700

    Chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,16 @@
 class SessionsController < ApplicationController
+  REMEMBER_ME_CONSTANT = "1".freeze
+
   # GET /login
   def new; end
 
   # POST /login
   def create
     user = User.find_by email: params.dig(:session, :email)&.downcase
-    if user&.authenticate params.dig(:session, :password)
-      log_in user
-      flash[:success] = t(".login_success")
-      redirect_to user, status: :see_other
+    if user.try(:authenticate, params.dig(:session, :password))
+      handle_successful_login user
     else
-      flash.now[:danger] = t(".invalid_email_password_combination")
-      render :new, status: :unprocessable_entity
+      handle_failed_login
     end
   end
 
@@ -19,5 +18,23 @@ class SessionsController < ApplicationController
   def destroy
     log_out
     redirect_to root_url, status: :see_other
+  end
+
+  private
+  def handle_successful_login user
+    reset_session
+    log_in user
+    if params.dig(:session,
+                  :remember_me) == REMEMBER_ME_CONSTANT
+      remember(user)
+    else
+      forget(user)
+    end
+    redirect_to user, status: :see_other
+  end
+
+  def handle_failed_login
+    flash.now[:danger] = t(".invalid_email_password_combination")
+    render :new, status: :unprocessable_entity
   end
 end
